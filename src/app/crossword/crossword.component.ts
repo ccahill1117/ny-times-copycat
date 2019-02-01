3704eb40e679b97ed445668c3af4874caa6f286c
Christopher Cahill <cthomascahill@gmail.com>
2019-02-01 08:11:53 -0800

    make xword password protected

--- src/app/crossword/crossword.component.ts
+++ src/app/crossword/crossword.component.ts
@@ -1,13 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../authentication.service';
 
 @Component({
   selector: 'app-crossword',
   templateUrl: './crossword.component.html',
-  styleUrls: ['./crossword.component.css']
+  styleUrls: ['./crossword.component.css'],
+  providers: [AuthenticationService]
 })
 export class CrosswordComponent implements OnInit {
 
-  constructor() { }
+  private isLoggedIn: Boolean = null;
+
+  constructor(public authService: AuthenticationService) {
+    this.authService.user.subscribe(user => {
+      if (user == null) {
+        this.isLoggedIn = false;
+      } else {
+        this.isLoggedIn = true;
+      }
+    });
+  }
 
   ngOnInit() {
   }
